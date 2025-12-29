9b5b6612b5cd3c6c1883ed658a7d78f6cd6f6efb
Henry Alfredo Garcia Palacio <henrygarcia-15@hotmail.com>
2025-12-29 10:45:15 -0500

    se organiza login

--- src/app/layout/dashboard-layout/dashboard-layout.ts
+++ src/app/layout/dashboard-layout/dashboard-layout.ts
@@ -3,10 +3,12 @@ import { DashboardTopbar } from '../../componentes/dashboard/dashboard-topbar/da
 import { DashboardSidebar } from '../../componentes/dashboard/dashboard-sidebar/dashboard-sidebar';
 import { DashboardCards } from '../../componentes/dashboard/dashboard-cards/dashboard-cards';
 import { DashboardTable } from '../../componentes/dashboard/dashboard-table/dashboard-table';
+import { RouterOutlet } from "@angular/router";
+import { Route } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [DashboardSidebar, DashboardTopbar],
+  imports: [DashboardSidebar, DashboardTopbar, RouterOutlet],
   templateUrl: './dashboard-layout.html',
   styleUrl: './dashboard-layout.css',
 })
