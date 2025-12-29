771618c6bf276e5be2c84b7357fb25c6a2d78e3a
Henry Alfredo Garcia Palacio <henrygarcia-15@hotmail.com>
2025-12-29 17:30:52 -0500

    Se corrige logo sidebar

--- src/app/componentes/dashboard/dashboard-sidebar/dashboard-sidebar.ts
+++ src/app/componentes/dashboard/dashboard-sidebar/dashboard-sidebar.ts
@@ -4,9 +4,32 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-sidebar',
+  imports:[CommonModule],
   templateUrl: './dashboard-sidebar.html',
   styleUrl: './dashboard-sidebar.css',
 })
 export class DashboardSidebar {
+
+  showLogoutModal = false;
+
+  constructor(private authGuard: AuthGuard){}
+
+  logout(event: Event): void{
+    event.preventDefault();
+    this.authGuard.logout();
+  }
+
+  openLogoutModal(): void {
+    this.showLogoutModal = true;
+  }
+
+  closeLogoutModal(): void {
+    this.showLogoutModal = false;
+  }
+
+  confirmLogout(): void {
+    this.showLogoutModal = false;
+    this.authGuard.logout();
+  }
 
 }
